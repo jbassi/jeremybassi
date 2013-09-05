67a6810342cd24478f40c75c4812d9f8df66260f
Jeremy Bassi <bassi.jeremy@gmail.com>
2013-09-05 16:47:12 -0700

    added resume support for smaller screens and projects section

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-// Powers animation and effects of jeremybassi.com
-// Author: Jeremy Bassi
-
 $(document).ready(function() {
+  var windowWidth = $(window).width()
+  var smallNameShown = false
+  var projectIndex = 0
+
+  // Initial resume size adjustment
+  if(windowWidth < 1020) {
+    $('#left').hide()
+    $('#small-name').show()
+    smallNameShown = true
+    $('#resume').css({'width':'auto'})
+    $('#right').css({
+      'margin-left':'5px',
+      'float':'none',
+      'width':'100%',
+    })
+    $('#resume-arrow').css({'width':windowWidth})
+  }
+
   $('#initials').fadeIn(1200)
 
-  // SHOW SELECTIONS
+  // Show selections mouse listener
   $('#hover').mouseenter(function() {
     $('#selections').fadeIn(300)
   })
@@ -12,42 +27,57 @@ $(document).ready(function() {
     $('#selections').fadeOut(500)
   })
 
-  // CENTER DIV
-  jQuery.fn.center = function () {
-    this.css('position','absolute')
-    this.css('top', Math.max(0, (($(window).height() - $(this).outerHeight()) / 2) + $(window).scrollTop()) + 'px')
-    this.css('left', Math.max(0, (($(window).width() - $(this).outerWidth()) / 2) + $(window).scrollLeft()) + 'px')
-    return this
-  }
-
-  // INITIALLY CENTER INDEX
-  $('#index').center()
-
-  // ON WINDOW RESIZE
+  // Adjust resume on window resize 
   $(window).resize(function() {
-    $('#' + $('body > div:visible').attr('id')).center()
+    windowWidth = $(window).width()
+    if(windowWidth > 1020) {
+      $('#resume').css({'width':'1020px'})
+      $('#right').css({
+        'float':'right',
+        'width':'720px',
+      })
+      $('#left').show()
+      $('#resume-arrow').css({'width':'1020px'})
+      $('#small-name').hide()
+      smallNameShown = false
+    } else if(windowWidth < 1020) {
+      $('#left').hide()
+      if(!smallNameShown) {
+        $('#small-name').show()
+      }
+      $('#resume').css({'width':'auto'})
+      $('#right').css({
+        'margin-left':'5px',
+        'float':'none',
+        'width':'100%',
+      })
+      $('#resume-arrow').css({'width':windowWidth})
+    }
   })
 
-  // SELECTION ANIMATION
+  // Selection navigation control
   $('#selections div').click(function() {
     var index = $(this).index()
 
     $('#index').fadeOut('fast', function() {
-      if(index == 0)
+      if(index === 0)
       {
         $('#about').fadeIn('fast')
-        $('#about').center()
         document.title = 'Jeremy Bassi | About'
       }
-      else if(index == 1)
+      else if(index === 1)
       {
         $('#resume').fadeIn('fast')
         document.title = 'Jeremy Bassi | Resume'
       }
-      else if(index == 3)
+      else if(index === 2)
+      {
+        $('#projects').fadeIn('fast')
+        document.title = 'Jeremy Bassi | Projects'
+      }
+      else if(index === 3)
       {
         $('#contact').fadeIn('fast')
-        $('#contact').center()
         document.title = 'Jeremy Bassi | Contact'
       }
       else
@@ -58,40 +88,83 @@ $(document).ready(function() {
     })
   })
 
-  // NAVIGATE FROM ABOUT TO RESUME
+  // Project selection control
+  $('#project-selections div').click(function() {
+    projectIndex = $(this).index()
+
+    $('#project-arrow').animate({'left':'364px'},200,function() {
+      $('#return').fadeIn('fast')
+    })
+
+    $('#project-selections').fadeOut('fast', function() {
+      if(projectIndex === 0) {
+        $('#pweb').fadeIn('fast')
+        document.title = 'Project -- Personal Website'
+      }
+      else if(projectIndex === 1) {
+        $('#couplr').fadeIn('fast')
+        document.title = 'Project -- Couplr'
+      }
+    })
+  })
+
+  // Project return arrow navigation control
+  $('#return').click(function() {
+    if(projectIndex === 0) {
+      $('#pweb').fadeOut('fast')
+      $(this).fadeOut('fast', function() {
+        $('#project-selections').fadeIn('fast')
+        $('#project-arrow').animate({'left':'340px'},200)
+        document.title = 'Jeremy Bassi | Projects'
+      })
+    }
+    else if(projectIndex === 1) {
+      $('#couplr').fadeOut('fast')
+      $(this).fadeOut('fast', function() {
+        $('#project-selections').fadeIn('fast')
+        $('#project-arrow').animate({'left':'340px'},200)
+        document.title = 'Jeremy Bassi | Projects'
+      })
+    }
+  })
+
+  // Navigate from about to resume
   $('#to-resume').click(function() {
     $('#about').fadeOut('fast', function() {
       $('#resume').fadeIn('fast')
     })
   })
 
-  // NAVIGATE FROM ABOUT TO CONTACT
+  // Navigate from about to contact
   $('#to-contact').click(function() {
     $('#about').fadeOut('fast', function() {
       $('#contact').fadeIn('fast')
-      $('#contact').center()
     })
   })
 
-  // RESUME TOP DIV MOVEMENT
-  $(window).scroll(function(){
-    $('#left').stop().animate({'marginTop':
-      ($(window).scrollTop()) + 'px'}, {'duration': 400}, {'easing': 'easeOutQuad'})
-    $('#resume .arrow').stop().animate({'marginTop':
-      ($(window).scrollTop()) + 'px'}, {'duration': 400}, {'easing': 'easeOutQuad'})
+  // Navigate from about to projects
+  $('#to-projects').click(function() {
+    $('#about').fadeOut('fast', function() {
+      $('#projects').fadeIn('fast')
+    })
   })
 
-  // ARROW FUNCTON
+  // Handle arrow click page navigation 
   $('.arrow').click(function() {
     $(this).parent().fadeOut('fast', function() {
       $('#index').fadeIn('fast', function() {
         $('#selections').fadeIn('fast')
       })
       document.title = 'Jeremy Bassi'
+      $('#return').hide()
+      $('#pweb').hide()
+      $('#couplr').hide()
+      $('#project-selections').show()
+      $('#project-arrow').css({'left':'340px'})
     }) 
   })
 
-  // TEXT HOVERING
+  // Text hovering 
   $('.fade').hover(function() {
       $(this).fadeTo('fast', 1) 
     }, function() {
